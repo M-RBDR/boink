e8084146d42ea4da123767d75acff8de01f7a323
M-RBDR <brundelrer@csdraveurs.qc.ca>
2021-08-10 01:21:38 -0400

    Ajout de commentaires

--- palette.py
+++ palette.py
@@ -10,19 +10,22 @@ class Palette(Sprite):
     def __init__(self, jeu, position):
         """Initialiser la palette et l'associer au jeu."""
         super().__init__()
+        # Associer les attributs du jeu à la palette
         self.ecran = jeu.ecran
         self.ecran_rect = jeu.ecran.get_rect()
         self.parametres = jeu.parametres
         self.couleur = self.parametres.couleur_palette
 
-        # Variables de gestion du mouvement
+        # Variables de gestion du mouvement afin d'assurer un mouvement continu
         self.mouvement_haut = False
         self.mouvement_bas = False
 
-        # Créer un rect pour une palette et le mettre à la bonne position.
+        # Créer un rect pour une palette et le mettre à la bonne position
         self.rect = pygame.Rect(0, 0, self.parametres.palette_largeur,
                                 self.parametres.palette_hauteur)
 
+        # Initialiser la position initiale de la palette à gauche ou à droite
+        # de l'écran selon le paramètre position
         if position == 'gauche':
             self.rect.midleft = self.ecran_rect.midleft
             self.rect.x += self.parametres.palette_largeur
@@ -31,12 +34,10 @@ class Palette(Sprite):
             self.rect.x -= self.parametres.palette_largeur
 
 
-        # Rendre la postion de la palatte en valeur décimale
-        self.y = float(self.rect.y)
-
-
     def mise_a_jour(self):
-        """Mettre à jour la position de la pallete selon la direction du mouvement"""
+        """Mettre à jour la position de la pallete
+        selon la direction du mouvement."""
+        # S'assurer que la palette reste dans l'espace de jeu
         if self.mouvement_haut and (self.rect.top > self.ecran_rect.top):
             self.rect.y -= 1
         if self.mouvement_bas and (self.rect.bottom < self.ecran_rect.bottom):
@@ -44,5 +45,5 @@ class Palette(Sprite):
 
 
     def afficher_palette(self):
-        """Afficher la palette à l'écran"""
+        """Afficher la palette à l'écran."""
         pygame.draw.rect(self.ecran, self.couleur, self.rect)
